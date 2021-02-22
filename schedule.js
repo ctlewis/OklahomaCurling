655d31c738af66bdd07070889b36f9e610552ce2
Chris <lewisengineer@gmail.com>
2021-02-21 20:57:06 -0700

    Add Support for Loading From Google Sheets

--- schedule.js
+++ schedule.js
@@ -1,37 +1,32 @@
 function arrayToTable(tableData) {
     var table = $('<table class="table table-striped text-center"></table>');
-    $(tableData).each(function (i, rowData) {
+    var i = 0;
+    tableData.forEach(function(rowData){
         //If this is the first row, treat it as a header
         if(i===0){
             var row = $('<thead class="thead-dark"><tr></tr></thead>');
-            $(rowData).each(function (j, cellData) {
+            rowData.forEach(function(cellData){
                 row.append($('<th scope="col">'+cellData+'</th>'));
             });}
         else{
             var row = $('<tr></tr>');
-            $(rowData).each(function (j, cellData) {
+            var j = 0;
+            rowData.forEach(function(cellData){
                 if(j===0){row.append($('<th scope="row">'+cellData+'</th>'));}
                 else{row.append($('<td>'+cellData+'</td>'));}
+                j++;
             });}
+            i++;
         
         table.append(row);
     });
     return table;
 }
 
-$.ajax({
-    type:"GET",
-    url: "http://www.okcurling.com/resources/schedule1.csv",
-    dataType: "text",
-    success: function (data) {
-        $('#draw1').append(arrayToTable(Papa.parse(data).data));
-    }
+$.getJSON ("https://sheets.googleapis.com/v4/spreadsheets/1zondef4UYwioMdHruLIVMuJjj96aLdnn25vvgFEPRTI/values/Draw1!A2:G20?key=AIzaSyBDKCSGO39d8AVVGoTRDKtVjYL7-1ux3Jg", function(result){
+    $('#draw1').append(arrayToTable(result.values))
 });
-$.ajax({
-    type:"GET",
-    url: "http://www.okcurling.com/resources/schedule2.csv",
-    dataType: "text",
-    success: function (data) {
-        $('#draw2').append(arrayToTable(Papa.parse(data).data));
-    }
+
+$.getJSON ("https://sheets.googleapis.com/v4/spreadsheets/1zondef4UYwioMdHruLIVMuJjj96aLdnn25vvgFEPRTI/values/Draw2!A2:G20?key=AIzaSyBDKCSGO39d8AVVGoTRDKtVjYL7-1ux3Jg", function(result){
+    $('#draw2').append(arrayToTable(result.values))
 });
--- standings.js
+++ standings.js
@@ -1,10 +1,11 @@
 function arrayToTable(tableData) {
     var table = $('<table class="table table-striped text-center"></table>');
-    $(tableData).each(function (i, rowData) {
+    var i = 0;
+    tableData.forEach(function(rowData){
         //If this is the first row, treat it as a header
         if(i===0){
             var row = $('<thead class="thead-dark"><tr></tr></thead>');
-            $(rowData).each(function (j, cellData) {
+            rowData.forEach(function(cellData) {
                 row.append($('<th scope="col">'+cellData+'</th>'));
             });}
         else{
@@ -23,29 +24,23 @@ function arrayToTable(tableData) {
             }
             
             var row = $('<tr class=' + color + '></tr>');
-            $(rowData).each(function (j, cellData) {
+            var j = 0;
+            rowData.forEach(function(cellData) {
                 if(j===0){row.append($('<th scope="row">'+cellData+'</th>'));}
                 else{row.append($('<td>'+cellData+'</td>'));}
+                j++;
             });}
         
         table.append(row);
+        i++;
     });
     return table;
 }
 
-$.ajax({
-    type:"GET",
-    url: "http://www.okcurling.com/resources/standings1.csv",
-    dataType: "text",
-    success: function (data) {
-        $('#draw1').append(arrayToTable(Papa.parse(data).data));
-    }
+$.getJSON ("https://sheets.googleapis.com/v4/spreadsheets/1Obk0rj1LvyCW9G9d57C4flvnmS6tHkE2iEQcnjiBBaM/values/Draw1!A2:F20?key=AIzaSyBDKCSGO39d8AVVGoTRDKtVjYL7-1ux3Jg", function(result){
+    $('#draw1').append(arrayToTable(result.values))
 });
-$.ajax({
-    type:"GET",
-    url: "http://www.okcurling.com/resources/standings2.csv",
-    dataType: "text",
-    success: function (data) {
-        $('#draw2').append(arrayToTable(Papa.parse(data).data));
-    }
+
+$.getJSON ("https://sheets.googleapis.com/v4/spreadsheets/1Obk0rj1LvyCW9G9d57C4flvnmS6tHkE2iEQcnjiBBaM/values/Draw2!A2:F20?key=AIzaSyBDKCSGO39d8AVVGoTRDKtVjYL7-1ux3Jg", function(result){
+    $('#draw2').append(arrayToTable(result.values))
 });
